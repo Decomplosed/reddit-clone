d1f4ff6f9bfdc3b24f9f35104fa155b58883801c
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-16 22:05:32 +0100

    extract email, username, password from req.body

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,3 +1,5 @@
 import { Request, Response } from 'express';
 
-const register = (req: Request, res: Response) => {};
+const register = (req: Request, res: Response) => {
+  const { email, username, password } = req.body;
+};
