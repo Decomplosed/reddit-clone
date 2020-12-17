c1a3a32fc276f07f13bda0f204e8b5db7e931cce
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-17 17:46:27 +0100

    imported validate from class-validator

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express';
+import { validate } from 'class-validator';
 import { User } from '../entities/User';
 
 const register = async (req: Request, res: Response) => {
