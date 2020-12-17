d55ec7a65e6fe807c5c63b94b9bc379d2a2a6c97
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-17 17:33:41 +0100

    return 500 if there's any error and show error

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -15,6 +15,7 @@ const register = async (req: Request, res: Response) => {
     // TODO: Return user
   } catch (error) {
     console.log(error);
+    return res.status(500).json(error);
   }
 };
 
