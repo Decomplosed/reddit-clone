985e03cd8702de91d085987720c1a5a8a4184313
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-17 17:29:19 +0100

    removed default columns from User entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -8,13 +8,4 @@ export class User extends BaseEntity {
   }
   @PrimaryGeneratedColumn()
   id: number;
-
-  @Column()
-  firstName: string;
-
-  @Column()
-  lastName: string;
-
-  @Column()
-  age: number;
 }
