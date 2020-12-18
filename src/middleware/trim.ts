23e2b0b14a39d62905db79c97ef2e70ecc500acd
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-18 23:44:38 +0100

    pass exceptions within if statement

--- src/middleware/trim.ts
+++ src/middleware/trim.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction) => {
+  const exceptions = ['password'];
+
   Object.keys(req.body).forEach((key) => {
-    if (typeof req.body[key] === 'string') {
-      const exceptions = ['password'];
+    if (!exceptions.includes(key) && typeof req.body[key] === 'string') {
       req.body[key] = req.body[key].trim();
     }
   });
