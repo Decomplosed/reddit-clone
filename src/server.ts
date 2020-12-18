2216bc5822f47ca6b21020ca8842f62ac2f92451
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-18 23:45:34 +0100

    use trim

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(trim);
 
 app.get('/', (_, res) => res.send('Hello there!'));
 app.use('/api/auth', authRoutes);
