793439ea71ffd63d8cbfff73f954207ab7b23c9b
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-18 23:43:02 +0100

    trim the keys that are string

--- src/middleware/trim.ts
+++ src/middleware/trim.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export default (req: Request, res: Response, next: NextFunction) => {
   Object.keys(req.body).forEach((key) => {
     if (typeof req.body[key] === 'string') {
+      req.body[key] = req.body[key].trim();
     }
   });
 };
