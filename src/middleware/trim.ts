fc75ed0b27f0f3e98b17b58beb301167f1c02ee4
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-18 23:42:37 +0100

    check if key from req.body is string

--- src/middleware/trim.ts
+++ src/middleware/trim.ts
@@ -1,3 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
-export default (req: Request, res: Response, next: NextFunction) => {};
+export default (req: Request, res: Response, next: NextFunction) => {
+  Object.keys(req.body).forEach((key) => {
+    if (typeof req.body[key] === 'string') {
+    }
+  });
+};
