f8a66e5765f727f625998129b186666bc2e77991
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-19 23:40:17 +0100

    declared errors let

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -39,6 +39,8 @@ const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
   try {
+    let errors: any = {};
+    
     const user = await User.findOne({ username });
     if (!user) return res.status(404).json({ error: 'User not found' });
 
