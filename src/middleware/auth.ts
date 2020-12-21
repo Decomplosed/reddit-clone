5d3c1e748c3fb1d0a78dc83ebdc71de2d684ed89
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-21 22:37:32 +0100

    copied code from me function

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,8 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction) => {
   try {
-    
-  } catch (error) {
+    const token = req.cookies.token;
+    if (!token) throw new Error('Unauthenticated');
 
-  }
+    const { username }: any = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ username });
+    if (!user) throw new Error('Unauthenticated');
+  } catch (error) {}
 };
