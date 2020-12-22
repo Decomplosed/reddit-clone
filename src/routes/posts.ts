d90372a501fe5799fadbb3e3635582e9075a1958
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-22 07:48:33 +0100

    get  title, body, sub from req.body

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,3 +1,5 @@
 import { Request, Response } from 'express';
 
-const createPost = (req: Request, res: Response) => {};
+const createPost = (req: Request, res: Response) => {
+  const { title, body, sub } = req.body;
+};
