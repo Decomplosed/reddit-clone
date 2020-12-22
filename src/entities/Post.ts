8b9a93d2e2ea21e576f53be8e9cc2fef42cb8458
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-22 07:33:57 +0100

    copied User code to the Post entity

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,6 +1,34 @@
-import {Entity} from "typeorm";
+import { IsEmail, Length } from 'class-validator';
+import { Entity as TOEntity, Column, Index, BeforeInsert } from 'typeorm';
+import bcrypt from 'bcrypt';
+import { Exclude } from 'class-transformer';
 
-@Entity()
-export class Post {
+import Entity from './Entity';
 
+@TOEntity('users')
+export default class User extends Entity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
+
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string;
+
+  @Index()
+  @Length(3, 255, { message: 'Username must be at least 3 characters long' })
+  @Column({ unique: true })
+  username: string;
+
+  @Exclude()
+  @Column()
+  @Length(6, 255)
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
 }
