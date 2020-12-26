53d6446b9b6d54fd7b63b1534d72d9b1972dd009
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-26 21:47:55 +0100

    imported Column

--- src/entities/Sub.ts
+++ src/entities/Sub.ts
@@ -1,4 +1,4 @@
-import { Entity as TOEntity } from 'typeorm';
+import { Column, Entity as TOEntity } from 'typeorm';
 import { slugify, makeId } from '../utils/helpers';
 
 import Entity from './Entity';
