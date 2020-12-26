ac48b69658b717f62e193e65db2b8dd146869bdc
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-26 21:46:39 +0100

    removed unused code

--- src/entities/Sub.ts
+++ src/entities/Sub.ts
@@ -1,47 +1,12 @@
-import {
-  Entity as TOEntity,
-  Column,
-  Index,
-  ManyToOne,
-  JoinColumn,
-  BeforeInsert,
-} from 'typeorm';
+import { Entity as TOEntity } from 'typeorm';
 import { slugify, makeId } from '../utils/helpers';
 
 import Entity from './Entity';
-import User from './User';
 
 @TOEntity('posts')
 export default class Post extends Entity {
   constructor(post: Partial<Post>) {
     super();
     Object.assign(this, post);
-  }
-
-  @Index()
-  @Column()
-  identifier: string; // 7 character ID
-
-  @Column()
-  title: string;
-
-  @Index()
-  @Column()
-  slug: string;
-
-  @Column({ nullable: true, type: 'text' })
-  body: string;
-
-  @Column()
-  subName: string;
-
-  @ManyToOne(() => User, (user) => user.posts)
-  @JoinColumn({ name: 'username', referencedColumnName: 'username' })
-  user: User;
-
-  @BeforeInsert()
-  makeIdAndSlug() {
-    this.identifier = makeId(7);
-    this.slug = slugify(this.title);
   }
 }
