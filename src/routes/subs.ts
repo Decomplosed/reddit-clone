4c8f5acf293e6e5dd0163156f8290e01b173f6e0
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-27 21:57:42 +0100

    extract name, title, description from req.body

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,3 +1,5 @@
 import { Request, Response } from 'express';
 
-const createSub = async (req: Request, res: Response) => {};
+const createSub = async (req: Request, res: Response) => {
+  const { name, title, description } = req.body;
+};
