c51736cf82b4a5b68548d3090e1344ec683e0994
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-27 21:59:09 +0100

    imported Sub entity

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import User from '../entities/User';
+import Sub from '../entities/Sub';
 
 const createSub = async (req: Request, res: Response) => {
   const { name, title, description } = req.body;
