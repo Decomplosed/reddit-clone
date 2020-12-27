091604fdd74e65cc3e0e9a1b56429039cf69984f
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-27 22:01:09 +0100

    added try catch block

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -7,6 +7,9 @@ import Sub from '../entities/Sub';
 const createSub = async (req: Request, res: Response) => {
   const { name, title, description } = req.body;
   const user: User = res.locals.user;
+
+  try {
+  } catch (error) {}
 };
 
 const router = Router();
