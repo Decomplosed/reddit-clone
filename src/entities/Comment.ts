4b7c11ef2e107e0cb2b07047d7dfb0814a4abea5
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-28 08:52:23 +0100

    added constructor for Comment entity

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -3,4 +3,9 @@ import { Entity as TOEntity } from 'typeorm';
 import Entity from './Entity';
 
 @TOEntity('comments')
-class Comments extends Entity {}
+class Comment extends Entity {
+  constructor(comment: Partial<Comment>) {
+    super();
+    Object.assign(this, comment);
+  }
+}
