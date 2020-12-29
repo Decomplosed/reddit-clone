21b40879606e2fc16ac8fe9033008c268f9e10f7
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-29 22:59:46 +0100

    export out Comment

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -3,7 +3,7 @@ import { Entity as TOEntity } from 'typeorm';
 import Entity from './Entity';
 
 @TOEntity('comments')
-class Comment extends Entity {
+export default class Comment extends Entity {
   constructor(comment: Partial<Comment>) {
     super();
     Object.assign(this, comment);
