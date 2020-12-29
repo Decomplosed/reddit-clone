645c541f3f668a6131b1f7a23ce223e544c4c7d6
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-29 23:07:17 +0100

    formatting fix

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,4 +1,10 @@
-import { BeforeInsert, Column, Entity as TOEntity, JoinColumn, ManyToOne } from 'typeorm';
+import {
+  BeforeInsert,
+  Column,
+  Entity as TOEntity,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
 
 import Entity from './Entity';
 import Post from './Post';
