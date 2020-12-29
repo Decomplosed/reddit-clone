e12a9f427875ee391faab725c307b1c67e7f3d1f
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-29 23:10:07 +0100

    imported Index

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,4 +1,10 @@
-import { Column, Entity as TOEntity, JoinColumn, ManyToOne } from 'typeorm';
+import {
+  Column,
+  Entity as TOEntity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
 
 import Entity from './Entity';
 import Post from './Post';
