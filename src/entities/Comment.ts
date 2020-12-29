9a1eb6298d364779c004bde58784dfb9ebcef87f
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-29 23:09:27 +0100

    removed BeforeInsert

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,10 +1,4 @@
-import {
-  BeforeInsert,
-  Column,
-  Entity as TOEntity,
-  JoinColumn,
-  ManyToOne,
-} from 'typeorm';
+import { Column, Entity as TOEntity, JoinColumn, ManyToOne } from 'typeorm';
 
 import Entity from './Entity';
 import Post from './Post';
@@ -32,6 +26,4 @@ export default class Comment extends Entity {
 
   @ManyToOne(() => Post, (post) => post.comments)
   post: Post;
-
-  @BeforeInsert()
 }
