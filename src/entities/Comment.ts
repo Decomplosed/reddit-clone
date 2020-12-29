e7cc427c29bf421f42693a8f8e9f44ec5a351fb2
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-29 23:04:09 +0100

    imported Post entity

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,6 +1,7 @@
 import { Column, Entity as TOEntity, JoinColumn, ManyToOne } from 'typeorm';
 
 import Entity from './Entity';
+import Post from './Post';
 import User from './User';
 
 @TOEntity('comments')
