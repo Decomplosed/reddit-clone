0f67159026262e160120309e759ff82bf466fec6
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-30 23:29:32 +0100

    removed import of the styles and added flex class to main div

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Register() {
   return (
-    <div className={styles.container}>
+    <div className='flex'>
       <Head>
         <title>Register</title>
         <link rel='icon' href='/favicon.ico' />
