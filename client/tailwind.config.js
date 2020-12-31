79066889ecd36c55cf882950009bb310927fab8d
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-31 23:08:13 +0100

    fixed the config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,14 +2,14 @@ module.exports = {
   purge: ['./src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
       spacing: {
         70: '17.5rem',
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
