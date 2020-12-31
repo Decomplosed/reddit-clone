75da29e92160a1d8fec3fb95917dddc07d098024
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-31 23:00:13 +0100

    added paragrapg

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -13,6 +13,7 @@ export default function Register() {
       />
       <div className='flex flex-col justify-center pl-6'>
         <h1 className='mb-2 text-lg'>Sign Up</h1>
+        <p className="mb-10 text-xs"></p>
       </div>
     </div>
   );
