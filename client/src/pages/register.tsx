969a94e721d0679cc775bc4e5a79c5906a40a37a
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-02 19:05:47 +0100

    passed value of username

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -51,6 +51,7 @@ export default function Register() {
                 type='text'
                 placeholder='Username'
                 className='w-full p-3 px-3 py-2 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white'
+                value={username}
               />
             </div>
             <div className='mb-2'>
