619fabf858f4a1bde1c5190845c4060ddb041578
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-02 18:54:50 +0100

    refactored logic of mapping the errors and moved that to separated funtion

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,7 +7,12 @@ import cookie from 'cookie';
 import User from '../entities/User';
 import auth from '../middleware/auth';
 
-const mapErrors = (errors: Object[]) => {};
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints[0][1]);
+    return prev;
+  }, {});
+};
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
