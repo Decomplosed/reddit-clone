057713ee136fcf53b5bee172d0c93b2c20a43c7f
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-03 10:19:17 +0100

    imported axios within main app file

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import Axios from 'axios';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
