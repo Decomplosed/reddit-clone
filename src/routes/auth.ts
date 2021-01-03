b1cc0cc7d7ff60061206154b92fa2d1c55f39c90
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-03 10:31:07 +0100

    fixed error within mapErrors

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,7 +9,7 @@ import auth from '../middleware/auth';
 
 const mapErrors = (errors: Object[]) => {
   return errors.reduce((prev: any, err: any) => {
-    prev[err.property] = Object.entries(err.constraints[0][1]);
+    prev[err.property] = Object.entries(err.constraints)[0][1];
     return prev;
   }, {});
 };
