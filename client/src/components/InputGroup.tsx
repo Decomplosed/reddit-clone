da680b6a855897d7da82aa5dcfddd1fea8d2d5f6
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-03 10:40:41 +0100

    return code of the email input

--- client/src/components/InputGroup.tsx
+++ client/src/components/InputGroup.tsx
@@ -1,1 +1,17 @@
-const InputGroup: React.FC = () => {};
+const InputGroup: React.FC = () => {
+  return (
+    <div className='mb-2'>
+      <input
+        type='text'
+        placeholder='Email'
+        className={classNames(
+          'w-full p-3 px-3 py-2 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white',
+          { 'border-red-500': errors.email },
+        )}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <small className='font-medium text-red-600'>{errors.email}</small>
+    </div>
+  );
+};
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -57,19 +57,7 @@ export default function Register() {
                 I agree to get emails about cool stuff on Readit
               </label>
             </div>
-            <div className='mb-2'>
-              <input
-                type='text'
-                placeholder='Email'
-                className={classNames(
-                  'w-full p-3 px-3 py-2 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white',
-                  { 'border-red-500': errors.email },
-                )}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <small className='font-medium text-red-600'>{errors.email}</small>
-            </div>
+
             <div className='mb-2'>
               <input
                 type='text'
