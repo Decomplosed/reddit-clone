db69ed4b96105ec9a8d8c0645cb3d42c305914e3
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-04 22:06:09 +0100

    imported InputGroup

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -2,3 +2,5 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Axios from 'axios';
 import { useRouter } from 'next/router';
+
+import InputGroup from '../components/InputGroup';
