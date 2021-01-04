5be1cb68217c2ab76cc18a27cafb34076a11b3fd
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-04 21:56:35 +0100

    added InputGroup for password

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -74,7 +74,14 @@ export default function Register() {
               placeholder='Username'
               error={errors.username}
             />
-
+            <InputGroup
+              type='password'
+              className='mb-2'
+              value={password}
+              setValue={setPassword}
+              placeholder='Password'
+              error={errors.password}
+            />
             <button className='w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded'>
               Sign Up
             </button>
