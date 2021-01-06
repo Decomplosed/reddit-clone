deaf1b0ee7689abcee0f0915a25ae1faea1b1ff1
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-06 09:13:55 +0100

    setErrors on error

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -23,7 +23,9 @@ export default function Login() {
       });
 
       router.push('/')
-    } catch (error) {}
+    } catch (error) {
+      setErrors(error.response.data)
+    }
   };
 
   <div className='flex'>
