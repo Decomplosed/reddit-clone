0d6b113ae75ec71041e2855bca8dd9c84d2e13ec
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-07 23:23:35 +0100

    added white background to navbar

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Head>
         <title>Readit</title>
       </Head>
-      <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12'>
+      <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 bg-white'>
         {/*Logo and title*/}
         <div className='flex items-center'>
           <Link href='/'>
