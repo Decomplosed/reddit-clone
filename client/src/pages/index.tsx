a008a178f5a30153060550c1c743587dbfe33149
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-07 23:11:58 +0100

    imported reddit svg

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+import RedditLogo from '../images/reddit.svg';
+
 export default function Home() {
   return (
     <div className={styles.container}>
