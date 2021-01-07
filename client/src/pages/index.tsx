d7b547859f2820d30d55c5ff6e5f4012e94bca41
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-07 23:14:07 +0100

    passed Reddit logo within Link

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -13,7 +13,11 @@ export default function Home() {
       <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12'>
         {/*Logo and title*/}
         <div className='flex items-center'>
-          <Link href='/'></Link>
+          <Link href='/'>
+            <a>
+              <RedditLogo className='w-8 h-8 mr-2' />
+            </a>
+          </Link>
         </div>
         {/* Search Input */}
         {/* Auth buttons */}
