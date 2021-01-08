6464440586b4da210b0c399519e4e2782052c002
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-08 16:00:02 +0100

    imported RedditLogo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import RedditLogo from '../images/reddit.svg';
+
 const Navbar: React.FC = () => (
   <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white'>
     {/*Logo and title*/}
