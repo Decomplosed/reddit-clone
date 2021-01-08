0c4a161eb19d1d2a752f0281fb7f0a65ed283ab5
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-08 16:10:44 +0100

    added padding to the main div

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,11 +2,13 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <div className='pt-12'>
       <Head>
         <title>Readit</title>
       </Head>
-      <div className='container'></div>
+      <div className='container'>
+        <h1>Recent Posts</h1>
+      </div>
     </div>
   );
 }
