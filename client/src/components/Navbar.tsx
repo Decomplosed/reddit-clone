0967569ece911ae6d5ab983b1ef599a995d7af3d
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-08 16:03:59 +0100

    removed commented parts

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import RedditLogo from '../images/reddit.svg';
 
 const Navbar: React.FC = () => (
   <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white'>
-    {/*Logo and title*/}
     <div className='flex items-center'>
       <Link href='/'>
         <a>
@@ -15,7 +14,6 @@ const Navbar: React.FC = () => (
         <Link href='/'>readit</Link>
       </span>
     </div>
-    {/* Search Input */}
     <div className='flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white'>
       <i className='pl-4 pr-3 text-gray-500 fas fa-search' />
       <input
@@ -24,7 +22,6 @@ const Navbar: React.FC = () => (
         placeholder='Search'
       />
     </div>
-    {/* Auth buttons */}
     <div className='flex'>
       <Link href='/login'>
         <a className='w-32 py-1 mr-4 leading-5 hollow blue button'>Log In</a>
