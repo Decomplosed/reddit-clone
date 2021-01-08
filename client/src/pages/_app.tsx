f4d3c370352593249387ed4fbf0c5118be667c5a
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-08 16:00:58 +0100

    imported Navbar

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,6 +3,8 @@ import Axios from 'axios';
 
 import '../styles/tailwind.css';
 
+import Navbar from '../components/Navbar';
+
 Axios.defaults.baseURL = 'http://localhost:5005/api';
 Axios.defaults.withCredentials = true;
 
