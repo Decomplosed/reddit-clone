9c8aefbf2fbd7b9a5486b4d2e656faf1ae24dbf1
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-09 20:34:08 +0100

    setPosts to the data from the payload

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    Axios.get('/posts').then((res) => {});
+    Axios.get('/posts').then((res) => setPosts(res.data));
   }, []);
 
   return (
