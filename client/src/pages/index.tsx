f2a592301723ac8e43d7ac53aed8f6453eb4d5da
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-09 20:31:30 +0100

    added useEffect

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head';
 
 export default function Home() {
+  useEffect(() => {}, []);
+
   return (
     <div className='pt-12'>
       <Head>
         <title>Readit</title>
       </Head>
       <div className='container flex pr-4'>
-        <div className="w-160"></div>
+        <div className='w-160'></div>
       </div>
     </div>
   );
