0322f601def615e58d1f2bfd3d1bfd0a63c8d503
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-10 19:22:42 +0100

    added Share and Save links

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -70,6 +70,14 @@ export default function Home() {
                       </div>
                     </a>
                   </Link>
+                  <div className='px-1 py-1 mr-2 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200'>
+                    <i className='mr-1 fas fa-comment-alt fa-xs' />
+                    <span className='font-bold'>Share</span>
+                  </div>
+                  <div className='px-1 py-1 mr-2 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200'>
+                    <i className='mr-1 fas fa-comment-alt fa-xs' />
+                    <span className='font-bold'>Save</span>
+                  </div>
                 </div>
               </div>
             </div>
