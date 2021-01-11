577833079152d9fb0497c92c8586d7faf9401563
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-11 21:47:02 +0100

    imported Post entity

--- src/entities/Vote.ts
+++ src/entities/Vote.ts
@@ -1,6 +1,7 @@
 import { Column, Entity as TOEntity, JoinColumn, ManyToOne } from 'typeorm';
 
 import User from './User'
+import Post from './Post'
 import Entity from './Entity';
 
 @TOEntity('votes')
