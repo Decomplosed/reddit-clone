aaf67d991b1f3856671d4795fce379e03a9f6e7a
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-12 08:07:31 +0100

    check if the value of vote is -1, 0 or 1

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -5,6 +5,10 @@ const router = Router();
 
 const vote = (req: Request, res: Response) => {
   const { identifier, slug, commentIdentifier, value } = req.body;
+
+  if (![-1, 0, 1].includes(value)) {
+    return res.status(400).json({ value: 'Value must be -1, 0 or 1' });
+  }
 };
 
 router.post('/vote', auth, vote);
