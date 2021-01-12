75142dbabc26e10c7bef0ddec599cf9104cc8f12
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-12 08:11:15 +0100

    find post by identifier or slug

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -14,6 +14,7 @@ const vote = async (req: Request, res: Response) => {
 
   try {
     const user: User = res.locals.user;
+    let post = await Post.findOneOrFail({ identifier, slug });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: 'Something went wrong' });
