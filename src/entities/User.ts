ab3302c3b5269ca562172cc7bda28bcddd397319
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-13 22:04:56 +0100

    imported Vote entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -11,6 +11,7 @@ import { Exclude } from 'class-transformer';
 
 import Entity from './Entity';
 import Post from './Post';
+import Vote from './Vote';
 
 @TOEntity('users')
 export default class User extends Entity {
