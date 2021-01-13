60d5917d450a16ba54cbc2ffc70cb798dbd7991c
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-13 22:06:41 +0100

    set the userVote

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -72,6 +72,7 @@ export default class Post extends Entity {
   protected userVote: number;
   setUserVote(user: User) {
     const index = this.votes?.findIndex((v) => v.username === user.username);
+    this.userVote = index > -1 ? this.votes[index].value : 0;
   }
 
   @BeforeInsert()
