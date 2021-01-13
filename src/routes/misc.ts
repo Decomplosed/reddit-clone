d391ed68e3e6f435932d62d57fd218b44ed70b8c
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-13 12:46:08 +0100

    added check if value is equal to 0

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -36,6 +36,7 @@ const vote = async (req: Request, res: Response) => {
       } else vote.post = post;
 
       await vote.save();
+    } else if (value === 0) {
     }
   } catch (error) {
     console.log(error);
