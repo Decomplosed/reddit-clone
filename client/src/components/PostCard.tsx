492a963edde52496da516b95def8ea8d7fd402fe
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-14 10:07:36 +0100

    fixed the hover state for both upvote and downvote

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -23,10 +23,10 @@ export default function PostCard({ post }) {
     <div key={post.identifier} className='flex mb-4 bg-white roudned'>
       {/*Vote section*/}
       <div className='w-10 text-center bg-gray-200 rounded-l'>
-        <div className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300'>
+        <div className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'>
           <i className='icon-arrow-up'></i>
         </div>
-        <div className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'>
+        <div className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600'>
           <i className='icon-arrow-down'></i>
         </div>
       </div>
