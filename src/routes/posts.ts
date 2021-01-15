8a61f8867c1ff4a7e11f275d2b054a0c67da2df7
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-15 16:12:48 +0100

    imported user middleware within posts

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express';
 import Comment from '../entities/Comment';
-
 import Post from '../entities/Post';
 import Sub from '../entities/Sub';
+
 import auth from '../middleware/auth';
+import user from '../middleware/user';
 
 const createPost = async (req: Request, res: Response) => {
   const { title, body, sub } = req.body;
