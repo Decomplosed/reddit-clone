0ffeb6a1e2c1ef61adc007bbf8e3d9a9bc808322
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-15 16:09:03 +0100

    omit request

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 import User from '../entities/User';
 
-export default async (req: Request, res: Response, next: NextFunction) => {
+export default async (_: Request, res: Response, next: NextFunction) => {
   try {
     const user: User | undefined = res.locals.user;
 
