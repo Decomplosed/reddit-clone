d16f9926d2e6bf3c51d2e4f36620020965963d76
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-15 16:01:12 +0100

    added onClick handler to upvote container

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -52,7 +52,10 @@ export default function PostCard({
     <div key={identifier} className='flex mb-4 bg-white roudned'>
       {/*Vote section*/}
       <div className='w-10 text-center bg-gray-200 rounded-l'>
-        <div className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'>
+        <div
+          className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'
+          onClick={}
+        >
           <i className='icon-arrow-up'></i>
         </div>
         <div className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600'>
