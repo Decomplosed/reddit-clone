398938ba9566cb838e3322833b6ddbcc7049db69
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-15 15:59:52 +0100

    fixed the data

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -45,7 +45,7 @@ export default function PostCard({
   };
 
   return (
-    <div key={post.identifier} className='flex mb-4 bg-white roudned'>
+    <div key={identifier} className='flex mb-4 bg-white roudned'>
       {/*Vote section*/}
       <div className='w-10 text-center bg-gray-200 rounded-l'>
         <div className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'>
@@ -54,44 +54,44 @@ export default function PostCard({
         <div className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600'>
           <i className='icon-arrow-down'></i>
         </div>
-        <p className='text-xs font-bold'>{post.voteScore}</p>
+        <p className='text-xs font-bold'>{voteScore}</p>
       </div>
       {/*Post data section*/}
       <div className='w-full p-2'>
         <div className='flex items-center'>
-          <Link href={`r/${post.subName}`}>
+          <Link href={`r/${subName}`}>
             <Fragment>
               <img
                 src='https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
                 className='w-6 h-6 mr-1 rounded-full cursor-pointer'
               />
               <a className='text-xs font-bold cursor-pointer hover:underline'>
-                /r/{post.subName}
+                /r/{subName}
               </a>
             </Fragment>
           </Link>
           <p className='text-xs text-gray-500'>
             <span className='mx-1'>•</span>
-            <Link href={`/u/${post.username}`}>
-              <a className='mx-1 hover:underline'>/u/{post.username}</a>
+            <Link href={`/u/${username}`}>
+              <a className='mx-1 hover:underline'>/u/{username}</a>
             </Link>
-            <Link href={post.url}>
+            <Link href={url}>
               <a className='mx-1 hover:underline'>
-                {dayjs(post.createdAt).fromNow()}
+                {dayjs(createdAt).fromNow()}
               </a>
             </Link>
           </p>
         </div>
-        <Link href={post.url}>
-          <a className='my-1 text-lg font-medium'>{post.title}</a>
+        <Link href={url}>
+          <a className='my-1 text-lg font-medium'>{title}</a>
         </Link>
-        {post.body && <p className='my-1 text-sm'>{post.body}</p>}
+        {body && <p className='my-1 text-sm'>{body}</p>}
         <div className='flex'>
-          <Link href={post.url}>
+          <Link href={url}>
             <a>
               <ActionButton>
                 <i className='mr-1 fas fa-comment-alt fa-xs' />
-                <span className='font-bold'>{post.commentCount} Comments</span>
+                <span className='font-bold'>{commentCount} Comments</span>
               </ActionButton>
             </a>
           </Link>
