9b452ae75566cc9932c10039056e6e7d1ec42d64
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-17 10:29:59 +0100

    imported useAuthDispatch

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import Axios from 'axios';
 import { useRouter } from 'next/router';
 
+import { useAuthDispatch } from '../context/auth';
+
 import InputGroup from '../components/InputGroup';
 
 export default function Login() {
