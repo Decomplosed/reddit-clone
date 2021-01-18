75fee97f0964b224686edbd48a885e304750d303
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-18 22:38:53 +0100

    created async function of loadUser

--- client/src/context/auth.tsx
+++ client/src/context/auth.tsx
@@ -44,7 +44,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     defaultDispatch({ type, payload });
 
   useEffect(() => {
-    Axios.get('/auth/me');
+    async function loadUser() {}
   });
 
   return (
