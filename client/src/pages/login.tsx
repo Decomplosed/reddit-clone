9c428faede8aa53a5a2ee930ce0648ce061f5123
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-18 22:49:14 +0100

    extract authenticated from useAuthState

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -14,6 +14,7 @@ export default function Login() {
   const [errors, setErrors] = useState<any>({});
 
   const dispatch = useAuthDispatch();
+  const { authenticated } = useAuthState();
 
   const router = useRouter();
 
