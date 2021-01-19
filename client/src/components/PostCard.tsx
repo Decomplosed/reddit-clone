711d31764f0bea92c722501d5ba31ecc46c4399a
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-19 22:38:01 +0100

    moved voteScore in between arrows

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -61,6 +61,7 @@ export default function PostCard({
             })}
           />
         </div>
+        <p className='text-xs font-bold'>{voteScore}</p>
         <div
           className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600'
           onClick={() => vote(-1)}
@@ -71,7 +72,6 @@ export default function PostCard({
             })}
           />
         </div>
-        <p className='text-xs font-bold'>{voteScore}</p>
       </div>
       {/*Post data section*/}
       <div className='w-full p-2'>
