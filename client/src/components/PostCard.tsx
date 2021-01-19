4be45e665f4b57a1b591070bdafda98d11516192
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-19 22:37:48 +0100

    fixed styling of vote container

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -50,7 +50,7 @@ export default function PostCard({
   return (
     <div key={identifier} className='flex mb-4 bg-white roudned'>
       {/*Vote section*/}
-      <div className='w-10 text-center bg-gray-200 rounded-l'>
+      <div className='w-10 py-3 text-center bg-gray-200 rounded-l'>
         <div
           className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'
           onClick={() => vote(1)}
