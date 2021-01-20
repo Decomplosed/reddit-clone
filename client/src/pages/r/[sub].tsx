4385f1af0642d70251da303b588fa227d8649866
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-20 15:41:38 +0100

    get subName

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -3,5 +3,7 @@ import { useRouter } from 'next/router';
 export default function Sub() {
   const router = useRouter();
 
+  const subName = router.query.sub;
+
   return <h1 className='text-5xl'>{router.query.sub}</h1>;
 }
