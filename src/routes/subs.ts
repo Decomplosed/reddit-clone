5d603d8820664ec15f2b5cd25e5604fcfb9941bb
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-22 10:58:15 +0100

    added +router.post

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -67,11 +67,14 @@ const getSub = async (req: Request, res: Response) => {
   }
 };
 
-const uploadSubImage = async (req: Request, res: Response) => {};
+const uploadSubImage = async (req: Request, res: Response) => {
+
+};
 
 const router = Router();
 
 router.post('/', user, auth, createSub);
 router.get('/:name', user, getSub);
+router.post()
 
 export default router;
