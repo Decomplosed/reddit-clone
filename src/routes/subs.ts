a66c3678dfcfc0abcac0a99ba07316bd287bb95a
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-23 23:00:23 +0100

    get type

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -106,6 +106,7 @@ const upload = multer({
 const uploadSubImage = async (_: Request, res: Response) => {
   const sub: Sub = res.locals.sub;
   try {
+    const type = req.body.type;
   } catch (error) {}
 
   return res.json({ success: true });
