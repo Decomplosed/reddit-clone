dae3ef0c3f1475cb3ff5423f12f226ad8b75965c
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-23 23:03:49 +0100

    save sub

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -117,6 +117,8 @@ const uploadSubImage = async (req: Request, res: Response) => {
     } else if (type === 'banner') {
       sub.bannerUrn = req.file.fieldname;
     }
+
+    await sub.save();
   } catch (error) {}
 
   return res.json({ success: true });
