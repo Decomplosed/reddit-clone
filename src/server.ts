bf903ff4a185cf5d358f7c5e64c2ac1a35d71a2d
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-23 23:12:58 +0100

    use public directory

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,8 @@ app.use(
   }),
 );
 
+app.use(express.static('public'));
+
 app.get('/', (_, res) => res.send('Hello there!'));
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
