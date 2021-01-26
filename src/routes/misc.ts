43910836c491a8b0850f55f205222f3767cffb07
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-26 09:03:41 +0100

    execute the query

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -71,7 +71,8 @@ const topSubs = async (req: Request, res: Response) => {
       .leftJoin(Post, 'p', `s.name = p."subName"`)
       .groupBy('s.title, s.name, "imageUrl"')
       .orderBy(`"postCount"`, 'DESC')
-      .limit(5);
+      .limit(5)
+      .execute();
   } catch (error) {}
 };
 
