3fa72edeac2101b9664c9292d1c9285fb38368a4
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-27 15:28:58 +0100

    added paragraph

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -2,7 +2,9 @@ export default function Sidebar() {
   return (
     <div className='ml-6 w-80'>
       <div className='bg-white rounded'>
-        <div className='p-3 bg-blue-500 rounded-t'></div>
+        <div className='p-3 bg-blue-500 rounded-t'>
+          <p className='font-semibold text-white'>About Community</p>
+        </div>
       </div>
     </div>
   );
