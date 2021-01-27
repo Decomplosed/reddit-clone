eed2aae52323b01a4cc5bcc8f5adb8011e8fafae
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-27 15:28:37 +0100

    added top banner

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,7 +1,9 @@
 export default function Sidebar() {
   return (
     <div className='ml-6 w-80'>
-      <div className='bg-white rounded'></div>
+      <div className='bg-white rounded'>
+        <div className='p-3 bg-blue-500 rounded-t'></div>
+      </div>
     </div>
   );
 }
