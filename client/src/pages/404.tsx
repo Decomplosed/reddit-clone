c18f6f80a68a7b32ae9e12b9af60f717fab8546f
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-28 09:11:38 +0100

    added heading

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,3 +1,7 @@
 export default function NotFound() {
-  return <div className='flex flex-col items-center'></div>;
+  return (
+    <div className='flex flex-col items-center'>
+      <h1 className='mt-10 mb-4 text-5xl text-gray-800'></h1>
+    </div>
+  );
 }
