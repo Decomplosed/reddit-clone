897e6038663efd90fe42c230566445cae9340dc3
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-28 09:12:22 +0100

    added info within heading

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className='flex flex-col items-center'>
-      <h1 className='mt-10 mb-4 text-5xl text-gray-800'></h1>
+      <h1 className='mt-10 mb-4 text-5xl text-gray-800'>Page Not Found</h1>
     </div>
   );
 }
