df7f468b8d15ca356ce625f9d324cf4e885cf8b8
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-28 22:38:02 +0100

    imported Image

--- client/src/pages/r/sub/[identifier]/[slug].tsx
+++ client/src/pages/r/sub/[identifier]/[slug].tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function PostPage() {
   const router = useRouter();
