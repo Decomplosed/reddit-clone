12f170f833618862270b3d05f2c63b21222a4e7b
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-28 09:12:39 +0100

    added Link

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -2,6 +2,7 @@ export default function NotFound() {
   return (
     <div className='flex flex-col items-center'>
       <h1 className='mt-10 mb-4 text-5xl text-gray-800'>Page Not Found</h1>
+      <Link href='/'></Link>
     </div>
   );
 }
