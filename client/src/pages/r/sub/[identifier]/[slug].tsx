612bbccd17a1e69cd48c804494eb8e4d7cf5fb5d
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-29 19:17:53 +0100

    fixed import

--- client/src/pages/r/sub/[identifier]/[slug].tsx
+++ client/src/pages/r/sub/[identifier]/[slug].tsx
@@ -7,7 +7,7 @@ import { Post } from '../../../../types';
 import Sidebar from '../../../../components/Sidebar';
 import classNames from 'classnames';
 import Axios from 'axios';
-import { authState } from '../../../../context/auth';
+import { useAuthState } from '../../../../context/auth';
 
 export default function PostPage() {
   const router = useRouter();
