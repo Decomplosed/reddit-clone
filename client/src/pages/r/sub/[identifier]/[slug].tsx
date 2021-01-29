0f50d3538e16c5e8b6ba282b1844439f8c5f6e07
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-29 19:17:17 +0100

    imported authState

--- client/src/pages/r/sub/[identifier]/[slug].tsx
+++ client/src/pages/r/sub/[identifier]/[slug].tsx
@@ -7,6 +7,7 @@ import { Post } from '../../../../types';
 import Sidebar from '../../../../components/Sidebar';
 import classNames from 'classnames';
 import Axios from 'axios';
+import { authState } from '../../../../context/auth';
 
 export default function PostPage() {
   const router = useRouter();
