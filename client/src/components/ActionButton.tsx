7584dc82e6071f07e66c520db9efcaa1c160db93
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-30 12:40:19 +0100

    extraacted children from props

--- client/src/components/ActionButton.tsx
+++ client/src/components/ActionButton.tsx
@@ -1,4 +1,4 @@
-export default function ActionButton() {
+export default function ActionButton({ children }) {
   return (
     <div className='px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200'>
       {children}
