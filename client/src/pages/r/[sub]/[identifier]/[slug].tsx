b9235ef11d62e4db4b7ab7ee10d5af9a2b877099
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-30 12:17:04 +0100

    removed bottom section of post

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -115,31 +115,6 @@ export default function PostPage() {
                       </Link>
                     </p>
                   </div>
-                  {/* Post title */}
-                  <h1 className='my-1 text-xl font-medium'>{title}</h1>
-                  {/* Post body */}
-                  <p className='my-3 text-sm'>{body}</p>
-                  {/* Actions */}
-                  <div className='flex'>
-                    <Link href={url}>
-                      <a>
-                        <ActionButton>
-                          <i className='mr-1 fas fa-comment-alt fa-xs'></i>
-                          <span className='font-bold'>
-                            {commentCount} Comments
-                          </span>
-                        </ActionButton>
-                      </a>
-                    </Link>
-                    <ActionButton>
-                      <i className='mr-1 fas fa-share fa-xs'></i>
-                      <span className='font-bold'>Share</span>
-                    </ActionButton>
-                    <ActionButton>
-                      <i className='mr-1 fas fa-bookmark fa-xs'></i>
-                      <span className='font-bold'>Save</span>
-                    </ActionButton>
-                  </div>
                 </div>
               </div>
             )}
