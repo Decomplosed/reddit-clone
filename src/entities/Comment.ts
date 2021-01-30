21d262f7391edd0f6f08af0ff8dba057aad8b81b
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-30 13:06:25 +0100

    imported Expose

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -14,7 +14,7 @@ import User from './User';
 
 import { makeId } from '../utils/helpers';
 import Vote from './Vote';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
 @TOEntity('comments')
 export default class Comment extends Entity {
