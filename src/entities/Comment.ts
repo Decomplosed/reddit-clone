20eb7e9c6ca0368699d5940bf0231a3851703e81
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-30 13:06:40 +0100

    copied get voteScore logic to Comment entity

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -44,6 +44,10 @@ export default class Comment extends Entity {
   @OneToMany(() => Vote, (vote) => vote.comment)
   votes: Vote[];
 
+  @Expose() get voteScore(): number {
+    return this.votes?.reduce((prev, curr) => prev + (curr.value || 0), 0);
+  }
+
   protected userVote: number;
   setUserVote(user: User) {
     const index = this.votes?.findIndex((v) => v.username === user.username);
