59891145f0b6f854d71b8f167969f1cf916c2b2d
Bart Frackiewicz <decomplosed@gmail.com>
2021-01-31 09:10:16 +0100

    added flex shrink to the post comment section as well

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -80,7 +80,7 @@ export default function PostPage() {
               <>
                 <div className='flex'>
                   {/* Vote section */}
-                  <div className='w-10 py-3 text-center rounded-l'>
+                  <div className='flex-shrink-0 w-10 py-3 text-center rounded-l'>
                     {/* Upvote */}
                     <div
                       className='w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'
