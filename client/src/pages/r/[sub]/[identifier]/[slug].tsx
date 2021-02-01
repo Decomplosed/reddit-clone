14edf0b2bdc0d86367659044e05f4af0432b3046
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-01 11:43:22 +0100

    show different template based on authenticated value

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -148,7 +148,9 @@ export default function PostPage() {
                   </div>
                 </div>
                 {/* {Comment input area} */}
-                <div className='pl-10 pr-6 mb-4'></div>
+                <div className='pl-10 pr-6 mb-4'>
+                  {authenticated ? () : ()}
+                </div>
                 <hr />
                 {/* {Comments feed} */}
                 {comments?.map((comment) => (
