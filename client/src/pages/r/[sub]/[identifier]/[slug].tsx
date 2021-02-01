eeb5d3fca0d6cb64835660d4487b9850b441725a
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-01 11:56:23 +0100

    added container for comment input

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -150,7 +150,7 @@ export default function PostPage() {
                 {/* {Comment input area} */}
                 <div className='pl-10 pr-6 mb-4'>
                   {authenticated ? (
-                    <p>Comment input</p>
+                    <div></div>
                   ) : (
                     <div className='flex items-center justify-between px-2 py-4 border border-gray-200 rounded'>
                       <p className='font-semibold text-gray-500'>
