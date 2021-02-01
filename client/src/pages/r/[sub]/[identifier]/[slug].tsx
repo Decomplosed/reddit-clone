d680cc7edda3836959bc7c69ba73c5742b8892d9
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-01 12:09:32 +0100

    imported FormEvent

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -12,7 +12,7 @@ import ActionButton from '../../../../components/ActionButton';
 import Sidebar from '../../../../components/Sidebar';
 import { useAuthState } from '../../../../context/auth';
 import { Post, Comment } from '../../../../types';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 dayjs.extend(relativeTime);
 
