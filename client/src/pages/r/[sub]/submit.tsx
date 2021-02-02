bb46019b6ed1d40f64597ae181ed1adb1f52934a
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-02 23:39:51 +0100

    added placeholder

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -25,6 +25,7 @@ export default function Submit() {
               <input
                 type='text'
                 className='w-full px-3 py-2 border border-gray-300 rounded focus:outline-none'
+                placeholder='Title'
               />
             </div>
           </form>
