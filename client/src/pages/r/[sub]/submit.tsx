a156129762f21af7bd93ce035081f3be8aa0e2c3
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-02 23:31:41 +0100

    imported Sidebar

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -1,3 +1,9 @@
+import Sidebar from '../../../components/Sidebar';
+
 export default function Submit() {
-  return <div className='container flex pt-5'></div>;
+  return (
+    <div className='container flex pt-5'>
+      <div className='w-160'></div>
+    </div>
+  );
 }
