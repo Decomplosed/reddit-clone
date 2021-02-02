fd806311aa6d1e569743ab583bf397cb247baf93
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-02 23:32:54 +0100

    get sub and error using useSWR

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -2,6 +2,8 @@ import useSWR from 'swr';
 import Sidebar from '../../../components/Sidebar';
 
 export default function Submit() {
+  const { data: sub, error } = useSWR();
+
   return (
     <div className='container flex pt-5'>
       <div className='w-160'></div>
