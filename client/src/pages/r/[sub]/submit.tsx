1485fe7ceac7982a026be69fa44f5b8a930e750a
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-02 23:41:18 +0100

    passed value of title

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -27,6 +27,7 @@ export default function Submit() {
                 className='w-full px-3 py-2 border border-gray-300 rounded focus:outline-none'
                 placeholder='Title'
                 maxLength={300}
+                value={title}
               />
             </div>
           </form>
