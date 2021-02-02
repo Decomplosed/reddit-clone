bc25ea68331b09aa342e914df4e0b627a3c44edd
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-02 23:38:29 +0100

    setup onSubmit

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -20,7 +20,7 @@ export default function Submit() {
       <div className='w-160'>
         <div className='p-4 bg-white rounded'>
           <h1 className='mb-3 text-lg'>Submit a post to /r/{subName}</h1>
-          <form></form>
+          <form onSubmit={submitPost}></form>
         </div>
       </div>
       <Sidebar sub={sub} />
