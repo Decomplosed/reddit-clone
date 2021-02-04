ec3c2ba2b4ffaa15c866f4c232167e13a3ab0ca2
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-04 23:35:59 +0100

    imported FormEvent

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import useSWR from 'swr';
 import Sidebar from '../../../components/Sidebar';
 import { Sub } from '../../../types';
