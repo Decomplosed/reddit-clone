70fb72ccb8187723324461e66260e18c7f60c5bb
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-05 14:55:28 +0100

    imported Comment entity

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,5 @@
 import { Response, Request, Router } from 'express';
+import Comment from '../entities/Comment';
 import Post from '../entities/Post';
 import User from '../entities/User';
 import user from '../middleware/user';
