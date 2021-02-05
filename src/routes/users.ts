772284baff91f73cfe298c105ef8b525f63d10d3
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-05 15:01:18 +0100

    return 500 on error

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -41,6 +41,7 @@ const getUserSubmissions = async (req: Request, res: Response) => {
     return res.json({ user, submissions });
   } catch (error) {
     console.log(error);
+    return res.status(500).json({ error: 'Something went wrong' });
   }
 };
 
