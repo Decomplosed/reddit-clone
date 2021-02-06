439730317cbb83ea4c9861af75e4d8b21f0d533b
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-06 23:02:24 +0100

    render paragrapg

--- client/src/pages/u/[username].tsx
+++ client/src/pages/u/[username].tsx
@@ -34,6 +34,9 @@ export default function User() {
                     <div className='flex-shrink-0 w-10 py-4 text-center rounded-l'>
                       <i className='mr-1 text-gray-500 fas fa-comment-alt fa-xs'></i>
                     </div>
+                    <div className='w-full p-2'>
+                      <p className='mb-2 text-xs text-gray-500'></p>
+                    </div>
                   </div>
                 );
               }
