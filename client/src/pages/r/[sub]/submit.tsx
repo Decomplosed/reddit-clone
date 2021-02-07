0171a9905e42b18c1081ba8915314392a572471e
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-07 10:41:25 +0100

    get cookie

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -87,5 +87,6 @@ export default function Submit() {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   try {
+    const cookie = req.headers.cookie;
   } catch (error) {}
 };
