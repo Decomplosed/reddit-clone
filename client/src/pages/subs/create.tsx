1a555a3be71a341ff4b548a86332e21600ad86f8
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-09 16:35:08 +0100

    useState case for description

--- client/src/pages/subs/create.tsx
+++ client/src/pages/subs/create.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 export default function Create() {
   const [name, setName] = useState('');
   const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   return <div></div>;
 }
