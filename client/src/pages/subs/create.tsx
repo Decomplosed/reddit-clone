1064a13acba82657e6d20cda35631695a73ef6d3
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-09 16:41:44 +0100

    render form

--- client/src/pages/subs/create.tsx
+++ client/src/pages/subs/create.tsx
@@ -23,6 +23,9 @@ export default function Create() {
         <div className='w-98'>
           <h1 className='mb-2 text-lg font-medium'>Create a Community</h1>
           <hr />
+          <form>
+            
+          </form>
         </div>
       </div>
     </div>
