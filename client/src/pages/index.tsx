eb677d53c3c17daa1d236b28268b01cc81d9b26f
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-09 16:23:47 +0100

    wrap image inside anchor tag

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -36,13 +36,15 @@ export default function Home() {
                   className='flex items-center px-4 py-2 text-xs border-b'
                 >
                   <Link href={`/r/${sub.name}`}>
-                    <Image
-                      src={sub.imageUrl}
-                      className='rounded-full cursor-pointer'
-                      alt='Sub'
-                      width={(6 * 16) / 4}
-                      height={(6 * 16) / 4}
-                    />
+                    <a>
+                      <Image
+                        src={sub.imageUrl}
+                        className='rounded-full cursor-pointer'
+                        alt='Sub'
+                        width={(6 * 16) / 4}
+                        height={(6 * 16) / 4}
+                      />
+                    </a>
                   </Link>
                   <Link href={`/r/${sub.name}`}>
                     <a className='ml-2 font-bold hover:cursor-pointer'>
