7aaa26d71e2c92407bd3f84641156734e7298eba
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-10 23:35:15 +0100

    imported FormEvent

--- client/src/pages/subs/create.tsx
+++ client/src/pages/subs/create.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from 'next';
 import Axios from 'axios';
 import Head from 'next/head';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import classNames from 'classnames';
 
 export default function Create() {
