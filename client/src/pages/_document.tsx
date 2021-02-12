7f5961a9ac487e53b19fc561cd72f658cadda2ae
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-12 22:06:33 +0100

    setup the meta icon

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -17,6 +17,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <title>Readit</title>
+          <link rel='icon' type='image/svg+xml' href='/reddit.svg' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
             href='https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600&display=swap'
