edb39453d726210208d0a7623af1dbefb96de155
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-12 21:04:55 +0100

    setup isLoadingInitialState variable

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -24,6 +24,7 @@ export default function Home() {
   } = useSWRInfinite<Post[]>((index) => `/posts?page=${index}`);
 
   const posts: Post[] = data ? [].concat(...data) : [];
+  const isLoadingInitialState = !data && !error;
 
   useEffect(() => {
     if (!posts || posts.length === 0) return;
